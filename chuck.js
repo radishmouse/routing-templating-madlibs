c730ebdebb2cc0c79c4d4a4a2172c933fcb8e256
Chris Aquino <chrisaquino@radishmouse.local>
2020-01-06 10:55:47 -0500

    completes the bonus exercise

--- chuck.js
+++ chuck.js
@@ -1,10 +1,12 @@
 const axios = require('axios');
 function getJoke() {
     const jokeURL = `https://api.chucknorris.io/jokes/random?category=dev`;
-    axios.get(jokeURL)
-        .then(response => {
-            console.log(response.data.value);
-        });
+    // console.log('about to return the Promise Chain');
+    return axios.get(jokeURL).then(response => {
+        // console.log('About to return the joke text'); 
+        return response.data.value;
+    });
+    console.log('you will not see this print');
 }
 module.exports = {
     getJoke
--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 // gimme the http module
 const http = require('http');
 const chuck = require('./chuck');
-
+let text;
 const server = http.createServer((req, res) => {
     console.log(req.url);
     // get a joke
-    chuck.getJoke();
-    // chuck.getDadJoke();
-    // console log it
-    res.write(req.url);
-    res.end();
+
+    if (req.url.startsWith('/joke')) {
+        // slice it! /joke/
+        // || is also known as the "guard operator"
+        const name = req.url.slice(6) || "Arya Stark";
+        console.log(`the name is ${name}`);
+        chuck.getJoke()
+            .then((jokeText) => {
+                // console.log('I got the joke text!');
+                text = jokeText.replace('Chuck Norris', name);
+                // chuck.getDadJoke();
+                // console log it
+                res.write(text);
+                res.end();
+            })
+            .then(() => {
+                // console.log('another .then')
+                console.log(text);
+            })
+    } else {
+        res.end('no joke for you');
+    }
+
+
+    // console.log('after chuck.getJoke()')
+    // console.log(text);
     // res.end('yeah it works');
 });
 
