7caf89b2b7333d3c82ea2b9e3472bd223b3411b3
Chris Aquino <chrisaquino@radishmouse.local>
2020-01-06 13:53:02 -0500

    completes part 1 of madlib 1

--- madlib1.js
+++ madlib1.js
@@ -1,10 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+    const justTheName = req.url.slice(1); // Everything after the '/'
+
     res.writeHead(200, {
         'Content-Type': 'text/plain'
     });
-    res.end(req.url);
+    res.end(`Hello ${justTheName}`);
 });
 
 server.listen(3000);
